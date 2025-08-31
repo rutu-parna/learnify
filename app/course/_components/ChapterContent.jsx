3f6763dd7df757cbdd0ed9e2545fa7203e041fc5
sumit <sumitkosta0@gmail.com>
2025-09-01 02:32:21 +0530

    course content completed

--- app/course/_components/ChapterContent.jsx
+++ app/course/_components/ChapterContent.jsx
@@ -7,6 +7,7 @@ function ChapterContent({courseInfo}) {
   const courseContent = courseInfo?.courses?.courseContent;
   const {SelectedChapterIndex,setSelectedChapterIndex} = useContext(SelectedChapterIndexContext)
   const videoData = courseContent?.[SelectedChapterIndex]?.youtubeVideo;
+  const topics = courseContent?.[SelectedChapterIndex]?.courseData?.topics;
 
   return (
     <div className='p-10'>
@@ -19,9 +20,23 @@ function ChapterContent({courseInfo}) {
               <YouTube videoId={video?.videoId}
               opts={{
                 height:'250',
-                width:'400'
+                width:'400',
+                
               }}
               />
+            </div>
+          ))}
+        </div>
+
+        <div className='mt-7'>
+          {topics?.map((topic,index) =>(
+            <div key = {index} className='mt-10 p-5 bg-secondary rounded-2xl'>
+              <h2 className='font-bold text-2xl text-primary'>{index+1}. {topic?.topic}</h2>
+              <div dangerouslySetInnerHTML={{__html:topic?.content}}
+              style={{
+                lineHeight:'2.5'
+              }}
+              ></div>
             </div>
           ))}
         </div>
--- app/course/_components/ChapterListSidebar.jsx
+++ app/course/_components/ChapterListSidebar.jsx
@@ -16,14 +16,14 @@ function ChapterListSidebar({courseInfo}) {
     
 
   return (
-        <div className='w-80 bg-secondary h-screen p-5'>
+        <div className='w-80 bg-secondary h-screen p-5 '>
             <h2 className='my-3 font-bold text-xl'>Chapters ({courseContent?.length})</h2>
             <Accordion type="single" collapsible>
                 { courseContent?.map((chapter,index) => (
                     <AccordionItem value={chapter?.courseData?.chapterName} key = {index}
                     onClick={()=>setSelectedChapterIndex(index)}
                     >
-                        <AccordionTrigger className={'text-lg font-medium '}> {index+1}.{chapter?.courseData?.chapterName}</AccordionTrigger>
+                        <AccordionTrigger className={'text-lg font-medium truncate'}> {index+1}.{chapter?.courseData?.chapterName}</AccordionTrigger>
                         <AccordionContent>
                             <div className=''>
                                 {chapter?.courseData?.topics.map((topic,index) => (
